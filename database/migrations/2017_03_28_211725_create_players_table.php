84b54795c84e6e7d5b7d2c9b57c3272df6fdb9cb
Alan <akoruth95@gmail.com>
2017-03-28 17:39:48 -0400

    Finish required migration.

--- database/migrations/2017_03_28_211725_create_players_table.php
+++ database/migrations/2017_03_28_211725_create_players_table.php
@@ -14,12 +14,19 @@ class CreatePlayersTable extends Migration
     public function up()
     {
       Schema::create('players', function(Blueprint $table) {
-        $table->string('first') - default();
-        $table->string('last') - default('');
-        $table->integer('gamesPlayed') - default(0);
-        $table->integer('avgMinutes') - default(0);
-        $table->integer('ppg') - default(0);
-        $table->timestamps();
+        $table->string('first') -> default();
+        $table->string('last') -> default('');
+        $table->integer('gamesPlayed') -> default(0);
+        $table->integer('avgMinutes') -> default(0);
+        $table->integer('ppg') -> default(0);
+        $table->integer('rpg') -> default(0);
+        $table->integer('apg') -> default(0);
+        $table->integer('spg') -> default(0);
+        $table->integer('bpg') -> default(0);
+        $table->integer('tpg') -> default(0);
+        $table->float('fgPercent', 8, 2) -> default(0.00);
+        $table->float('ftPercent', 8, 2) -> default(0.00);
+        $table->float('tpPercent', 8, 2) -> default(0.00);
       });
 
     }
@@ -31,6 +38,6 @@ class CreatePlayersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('players');
     }
 }
